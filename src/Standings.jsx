b2f6c44931e2682e320bd6a549f1bda812203023
Noah F <nhford@umass.edu>
2025-01-02 14:28:07 -0500

    working sortable h2h table

--- src/Standings.jsx
+++ src/Standings.jsx
@@ -7,9 +7,9 @@ import PropTypes from 'prop-types';
 function Standings({sport}) {
   const [data,setData] = useState([]);
   const [expandedRow, setExpandedRow] = useState(null);
+  const [sorted,setSorted] = useState({key:"pct",dir:"asc"});
 
   const connection = 'api/fetch'
-
   useEffect(() => {
     // Fetch teams data from the API
     fetch(connection)
@@ -18,12 +18,11 @@ function Standings({sport}) {
       .catch(error => console.error('Error fetching data:', error));
   }, [sport]);
 
-  const [sorted,setSorted] = useState({key:"pct",dir:"asc"});
 
   const sortingUtil = [sorted,setSorted,data,setData];
 
-  const toggleExpand = (rowId) => {
-    setExpandedRow(expandedRow === rowId ? null : rowId); // Toggle expanded row
+  const toggleExpand = (rowIndex) => {
+    setExpandedRow(expandedRow === rowIndex ? null : rowIndex); // Toggle expanded row
   };
 
   return (
